5e020c8c95e67ee80540a18f37a1d7fd20847ed4
Aditya Singh <aditya@caesiumlab.tech>
2025-03-25 17:01:53 +0530

    feat: Added MM

--- Assets/StorePackages/Feel/MMTools/Editor/MMMovement/MMPathMovement/MMPathMovementEditor.cs
+++ Assets/StorePackages/Feel/MMTools/Editor/MMMovement/MMPathMovement/MMPathMovementEditor.cs
@@ -62,7 +62,7 @@ namespace MoreMountains.Tools
 				Handles.Label(t.PointPosition(i) + (Vector3.down*0.4f) + (Vector3.right*0.4f), ""+i,style);
 
 				// draws a movable handle
-				var fmh_65_57_638748054948175041 = Quaternion.identity; Vector3 newPoint = Handles.FreeMoveHandle(oldPoint,.5f,new Vector3(.25f,.25f,.25f),Handles.CircleHandleCap);
+				var fmh_65_57_638748101885472519 = Quaternion.identity; Vector3 newPoint = Handles.FreeMoveHandle(oldPoint,.5f,new Vector3(.25f,.25f,.25f),Handles.CircleHandleCap);
 
 				// records changes
 				if (EditorGUI.EndChangeCheck())
